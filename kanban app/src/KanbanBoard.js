ab9214a5cc185a75892fcc20b4c9d6ec820784ef
jjy <iamheroz@naver.com>
2020-05-06 09:29:34 +0900

    React Practics

--- kanban app/src/KanbanBoard.js
+++ kanban app/src/KanbanBoard.js
@@ -1,17 +1,16 @@
+
 import React from 'react';
 import CardList from './CardList'
-export default class KanbanBoard extends React.Component{
-    render(){
-        const allCards = this.props.cards;
-        let cards={
-            "todo" :[],
-            "in-progress" :[],
-            "done" :[]
-        }
-        allCards.forEach(function(card){
-          cards[card.status].push(card);
-        });
 
+export default class KanbanBoard extends React.Component {
+    render() {
+        const allCards = this.props.cards;
+        let cards = {
+            "todo": [],
+            "in-progress": [],
+            "done": []
+        }
+        allCards.forEach(card => cards[ card.status ].push( card ));
         return (
             <div className='KanbanBoard'>
                 <CardList key='todo' cards={ cards['todo'] } title='To Do' />
@@ -20,5 +19,4 @@ export default class KanbanBoard extends React.Component{
             </div>
         )
     }
-
 }
